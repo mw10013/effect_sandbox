2a1bdcbbb10256cc90160e8d49561e4c8f12f6c3
Michael Wu <mw10013@gmail.com>
2023-11-19 17:00:59 -0500

    .

--- src/fi.ts
+++ src/fi.ts
@@ -1,4 +1,13 @@
-import { Config, Console, Effect, LogLevel, Logger, pipe } from "effect";
+import {
+  Config,
+  ConfigError,
+  Console,
+  Context,
+  Effect,
+  Layer,
+  LogLevel,
+  Logger,
+} from "effect";
 
 // const contact = await (async (accessToken) => {
 //     if (!accessToken) {
@@ -14,7 +23,29 @@ import { Config, Console, Effect, LogLevel, Logger, pipe } from "effect";
 //     return await response.json();
 //   })(await getAccessToken({session, env}));
 
+interface HubspotService {
+  readonly getContact: () => Effect.Effect<
+    never,
+    ConfigError.ConfigError,
+    string
+  >;
+}
 
+const HubspotService = Context.Tag<HubspotService>("@app/HubspotService");
+
+const HubspotServiceLive = Layer.effect(
+  HubspotService,
+  Effect.gen(function* (_) {
+    const getContact: HubspotService["getContact"] = () =>
+      Effect.gen(function* (_) {
+        const accessToken = yield* _(
+          Effect.config(Config.string("HUBSPOT_PRIVATE_ACCESS_TOKEN"))
+        );
+        return accessToken;
+      });
+    return HubspotService.of({ getContact });
+  })
+);
 
 const fetchContact = (accessToken: string) =>
   Effect.tryPromise({
@@ -34,18 +65,21 @@ const getJson = (res: Response) =>
   });
 
 const program = Effect.gen(function* (_) {
-  const accessToken = yield* _(
-    Effect.config(Config.string("HUBSPOT_PRIVATE_ACCESS_TOKEN"))
-  );
-  yield* _(Effect.logDebug(`accessToken: ${accessToken}`));
-  const response = yield* _(fetchContact(accessToken));
-  //   console.log("response: %o", response);
-  const json = yield* _(getJson(response));
-  yield* _(Console.log("json: %o", json));
-  //   throw new Error("Woah");
+  const hubspotService = yield* _(HubspotService);
+  const contact = yield* _(hubspotService.getContact());
+  yield* _(Effect.logDebug(`contact: ${contact}`));
+
+  //   const accessToken = yield* _(
+  //     Effect.config(Config.string("HUBSPOT_PRIVATE_ACCESS_TOKEN"))
+  //   );
+  //   yield* _(Effect.logDebug(`accessToken: ${accessToken}`));
+  //   const response = yield* _(fetchContact(accessToken));
+  //   const json = yield* _(getJson(response));
+  //   yield* _(Console.log("json: %o", json));
 }).pipe(Logger.withMinimumLogLevel(LogLevel.Debug));
 
 const main = program.pipe(
+  Effect.provide(HubspotServiceLive),
   Effect.catchAll(() => Console.error("A dreadful error occurred"))
 );
 
